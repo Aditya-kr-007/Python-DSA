a7e340c0456f279973f4669904e9080e4e0dc249
Aditya-kr-007 <adikumar681@gmail.com>
2026-08-10 01:52:17 +0530

    add

---
+++ pow(x,n).py
@@ -0,0 +1,17 @@
+class Solution:
+    def pow(self,x,n):
+        if n==0:
+            return 1
+        a=self.pow(x,n//2)
+        if n%2==0:
+            return a*a
+        else:
+            return a*a*x
+    def myPow(self, x: float, n: int) -> float:
+        if n>=0:
+            return self.pow(x,n)
+        else:
+            return 1/self.pow(x,n*(-1))
+x=int(input("enter x of x^n= "))
+n=int(input("enter n of x^n= "))
+print(Solution().myPow(x,n))
