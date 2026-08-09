fb2a8aac96e2f399870fc04fdfa70b5024b3f28d
Aditya-kr-007 <adikumar681@gmail.com>
2026-08-09 23:43:11 +0530

    add

---
+++ fibonnaci.py
@@ -0,0 +1,8 @@
+def fib(n):
+        if n==0 or n==1:
+            return n
+        return fib(n-1)+fib(n-2)
+n=int(input("enter the nth term for fibo = "))
+print("the fibonacci number is=",fib(n))
+
+            
