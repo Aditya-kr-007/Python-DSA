b5f39bd163655323fc45fcf8bed660bdee1af772
Aditya-kr-007 <adikumar681@gmail.com>
2026-08-12 19:24:25 +0530

    update

---
+++ maxsubarray.py
@@ -0,0 +1,16 @@
+from typing import List
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        n=len(nums)
+        curr_sum=0
+        max_sum=nums[0]
+        for i in range(n):
+            curr_sum=curr_sum+nums[i]
+            if curr_sum>max_sum:
+                max_sum=curr_sum
+            if curr_sum<0:
+                curr_sum=0
+        return max_sum
+n=int(input("enter the list size= "))
+nums=list(map(int,input().split()))
+print(Solution().maxSubArray(nums))
