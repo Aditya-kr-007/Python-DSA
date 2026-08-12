711416e418f5360dc049bafdc02eac23da27bcce
Aditya-kr-007 <adikumar681@gmail.com>
2026-08-12 18:09:29 +0530

    add

--- 2timesrepeat.py
+++ 2timesrepeat.py
@@ -1,14 +1,12 @@
-from typing import List
-
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         n=len(nums)
-        k=0
-        for i in range(1,n):
-                if nums[i]!=nums[k]:
+        k=1
+        for i in range(2,n):
+                if nums[i]!=nums[k-1]:
                     k+=1
                     nums[k]=nums[i]
-        return k+1, nums[:k+1]
+        return k+1,nums[:k+1]
 n=int(input("enter the list size= "))
 nums=list(map(int,input().split()))
 print(Solution().removeDuplicates(nums))
