461d07a16210ac05173bb421702249d937071cd2
Aditya-kr-007 <adikumar681@gmail.com>
2026-08-13 02:22:54 +0530

    add

---
+++ 2darrayinput.py
@@ -0,0 +1,28 @@
+#PREFERED FORMAT
+rows = int(input("enter rows: "))
+cols = int(input("enter columns: "))
+
+matrix = []
+
+for i in range(rows):
+    row = list(map(int, input().split()))
+    matrix.append(row)
+
+print(matrix) 
+
+#LONG METHOD IN WHICH INPUT IS TAKEN ONE BY ONE BUT NOT WHOLE COLUMN OF EACH ROW AT ONCE.
+"""
+rows = int(input("enter rows: "))
+cols = int(input("enter cols: "))
+
+matrix = []
+
+for i in range(rows):
+    row = []
+    for j in range(cols):
+        c = int(input())
+        row.append(c)
+    matrix.append(row)
+
+print(matrix)
+"""
