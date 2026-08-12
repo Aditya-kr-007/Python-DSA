d872594c617d7300549181395b5b3c6886d9849a
Aditya-kr-007 <adikumar681@gmail.com>
2026-08-13 04:17:12 +0530

    add

--- defangIPaddr.py
+++ defangIPaddr.py
@@ -1,8 +1,15 @@
 class Solution:
     def defangIPaddr(self, address: str) -> str:
-        ans = address.replace(".", "[.]")
+        ans=""
+        for i in address:
+          if i!=".":
+             ans=ans+i
+          else:
+             ans=ans+"[.]"
         return ans
-
-
+"""
+        ans= address.replace(".","[.]")
+        return ans
+"""
 address = "192.168.1.1"
 print(Solution().defangIPaddr(address))
