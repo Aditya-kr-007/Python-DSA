0171a952c0d09f203999ce509a99d38919b1ab2f
Aditya-kr-007 <adikumar681@gmail.com>
2026-09-02 22:25:57 +0530

    add

---
+++ insertionsort.py
@@ -0,0 +1,17 @@
+
+def sortArray(nums):
+        
+        n=len(nums)
+         #iterate through the array starting from the second element
+        for i in range(1,n):
+            key = nums[i]    #store the current element in a variable called key
+            j = i-1   #initialize a variable j to the index of the previous element
+            #while j is greater than or equal to 0 and the element at index j is greater than key, shift the element at index j to the right by one position and decrement j by 1
+            while j>=0 and nums[j]>key:  
+                nums[j+1] = nums[j]       
+                j = j-1   #decrement j by 1
+            
+            nums[j+1]=key  #insert the key at the correct position in the sorted portion of the array
+        return nums
+nums=list(map(int,input("Enter the elements of the array separated by space: ").split()))
+print("the sorted array is ",sortArray(nums))
